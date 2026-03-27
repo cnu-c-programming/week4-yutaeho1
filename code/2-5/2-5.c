55e3acd2ad6ce88bd906414439d425344b0992ae
yutaeho1 <yut02@naver.com>
2026-03-27 12:04:53 +0900

    Update 2-5.c

---
+++ code/2-5/2-5.c
@@ -0,0 +1,19 @@
+    #include <stdio.h>
+    #include <math.h>
+
+    int main() {
+        double x = 9.0;
+        double y = 2.5;
+
+        /* sqrt: 제곱근 */
+        printf("%.2f\n", sqrt(x));
+        /* pow: 거듭제곱 */
+        printf("%.2f\n", pow(x, y));
+        /* ceil: 올림 */
+        printf("%.1f\n", ceil(y));
+        /* floor: 내림 */
+        printf("%.1f\n", floor(y));
+
+
+        return 0;
+    }
