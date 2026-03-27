5e454c8f27b336f81ebf0872c5678fce72b0262e
yutaeho1 <yut02@naver.com>
2026-03-27 12:01:48 +0900

    Update 2-4.c

---
+++ code/2-4/2-4.c
@@ -0,0 +1,20 @@
+    #include <stdio.h>
+    #include <ctype.h>
+
+    int main() {
+        char chars[] = { 'A', 'Z', '5', '!'};
+        int n = sizeof(chars) / sizeof(chars[0]);
+
+        for (int i=0; i < n; i++) {
+            char ch = chars[i];
+            printf("%c - alpha:%d digit:%d upper:%c lower:%c\n", 
+            ch, 
+            isalpha(ch), 
+            isdigit(ch),
+            toupper(ch),
+            tolower(ch));    
+                
+        }
+
+        return 0;
+    }
