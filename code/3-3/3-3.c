a1c2f1d0530e397b155526548a3708b8a1f671a6
yutaeho1 <yut02@naver.com>
2026-04-02 13:54:53 +0900

    Update 3-3.c

---
+++ code/3-3/3-3.c
@@ -0,0 +1,11 @@
+    #include <stdio.h>
+    
+    int main() {
+        
+        printf("sizeof(int*) = %zu\n", sizeof(int*));
+        printf("sizeof(double*) = %zu\n", sizeof(double*));
+        printf("sizeof(char*) = %zu\n", sizeof(char*));
+        printf("sizeof(void*) = %zu\n", sizeof(void*));
+
+        return 0;
+    }
