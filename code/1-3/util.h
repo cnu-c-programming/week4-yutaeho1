4acf23d9450432e91d38f282c47bbe39e79ed23c
yutaeho1 <yut02@naver.com>
2026-04-02 13:21:39 +0900

    Update util.h

--- code/1-3/util.h
+++ code/1-3/util.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-static inline void logger static_inline(char* message) {
+static inline void logger_static_inline(char* message) {
     static int counter = 0;
     printf("log %2d: %d\n", counter++, message);
 }
